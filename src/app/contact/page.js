dee0e448e4501efe2a2a4eae850e593cbb4b4202
RashiFofaria10 <d25dce154@charusat.edu.in>
2026-05-21 14:19:54 +0530

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,21 @@
 export default function ContactPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Contact Page
-      </h1>
+    <main className="min-h-screen bg-[#f6f9fc] flex items-center justify-center">
+      <div className="text-center">
+
+        <h1 className="text-5xl font-bold text-[#0b2d57] mb-10">
+          Contact Us
+        </h1>
+
+        <div className="space-y-5 text-xl text-gray-700">
+
+          <p>📞 +91 9426272081</p>
+          <p>✉ info@mukeshgraphics.com</p>
+          <p>📍 Bhavnagar, Gujarat</p>
+
+        </div>
+
+      </div>
     </main>
-  )
+  );
 }
