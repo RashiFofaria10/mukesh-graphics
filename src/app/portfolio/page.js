b96505c682588cd4529ca6a52185989c5f5cfac0
RashiFofaria10 <d25dce154@charusat.edu.in>
2026-05-21 14:19:27 +0530

    Update page.js

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,9 +1,17 @@
 export default function PortfolioPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Portfolio Page
-      </h1>
+    <main className="min-h-screen bg-white flex items-center justify-center">
+      <div className="text-center">
+
+        <h1 className="text-5xl font-bold text-[#0b2d57] mb-10">
+          Portfolio
+        </h1>
+
+        <p className="text-xl text-gray-600">
+          Premium packaging and printing projects.
+        </p>
+
+      </div>
     </main>
-  )
+  );
 }
