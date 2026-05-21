f0b4804b3117289c52fc7666093fd8eeeb968ca6
RashiFofaria10 <d25dce154@charusat.edu.in>
2026-05-21 14:18:59 +0530

    Update page.js

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,9 +1,21 @@
 export default function ServicesPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Services Page
-      </h1>
+    <main className="min-h-screen flex items-center justify-center bg-[#f6f9fc]">
+      <div className="text-center">
+
+        <h1 className="text-5xl font-bold text-[#0b2d57] mb-10">
+          Our Services
+        </h1>
+
+        <div className="space-y-6 text-xl text-gray-700">
+
+          <p>📦 Packaging Design</p>
+          <p>🖨 Offset Printing</p>
+          <p>🎨 Branding Solutions</p>
+
+        </div>
+
+      </div>
     </main>
-  )
+  );
 }
